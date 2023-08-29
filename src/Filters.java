5534d527b76a4c20d15a9f723a2edec78cbcff8a
Abhinav Gupta <Abhinav.gupta@outlook.de>
2023-08-30 02:29:02 +0530

    Implemented rotate, flip, blur

--- src/Filters.java
+++ src/Filters.java
@@ -52,13 +52,116 @@ public class Filters {
   return outputImage;
  }
 
+ public BufferedImage rotateNintyImage(BufferedImage inputImage) {
+  width = pixelValues.getWidth(inputImage);
+  height = pixelValues.getHeight(inputImage);
+  BufferedImage outputImage = new BufferedImage(height, width, BufferedImage.TYPE_3BYTE_BGR);
+  for (int i = 0; i < width; i++) {
+   for (int j = 0; j < height; j++) {
+    outputImage.setRGB(j, i, inputImage.getRGB(i, j));
+   }
+  }
+  return outputImage;
+ }
+
+ public BufferedImage horizontalFlip(BufferedImage inputImage) {
+  width = pixelValues.getWidth(inputImage);
+  height = pixelValues.getHeight(inputImage);
+  BufferedImage outputImage = new BufferedImage(width, height, BufferedImage.TYPE_3BYTE_BGR);
+  for (int i = 0; i < width; i++) {
+   for (int j = 0; j < height; j++) {
+    outputImage.setRGB(i, j, inputImage.getRGB(width - i - 1, j));
+   }
+  }
+  return outputImage;
+ }
+
+ public BufferedImage verticalFlip(BufferedImage inputImage) {
+  width = pixelValues.getWidth(inputImage);
+  height = pixelValues.getHeight(inputImage);
+  BufferedImage outputImage = new BufferedImage(width, height, BufferedImage.TYPE_3BYTE_BGR);
+  for (int i = 0; i < width; i++) {
+   for (int j = 0; j < height; j++) {
+    outputImage.setRGB(i, j, inputImage.getRGB(i, height - j - 1));
+   }
+  }
+  return outputImage;
+ }
+
+ public BufferedImage blurImage(BufferedImage inputImage) {
+  width = pixelValues.getWidth(inputImage);
+  height = pixelValues.getHeight(inputImage);
+  BufferedImage outputImage = new BufferedImage(width, height, BufferedImage.TYPE_3BYTE_BGR);
+
+  Color[] color = null;
+
+  int i = 0;
+  int max = 400, rad = 10;
+  int a1 = 0, r1 = 0, g1 = 0, b1 = 0;
+  color = new Color[max];
+
+  // Now this core section of code is responsible for
+  // blurring of an image
+
+  int x = 1, y = 1, x1, y1, d = 0;
+
+  // Running nested for loops for each pixel
+  // and blurring it
+  for (x = rad; x < inputImage.getHeight() - rad; x++) {
+   for (y = rad; y < inputImage.getWidth() - rad; y++) {
+    for (x1 = x - rad; x1 < x + rad; x1++) {
+     for (y1 = y - rad; y1 < y + rad; y1++) {
+      color[i++] = new Color(
+        inputImage.getRGB(y1, x1));
+     }
+    }
+
+    // Smoothing colors of image
+    i = 0;
+    for (d = 0; d < max; d++) {
+     a1 = a1 + color[d].getAlpha();
+    }
+
+    a1 = a1 / (max);
+    for (d = 0; d < max; d++) {
+     r1 = r1 + color[d].getRed();
+    }
+
+    r1 = r1 / (max);
+    for (d = 0; d < max; d++) {
+     g1 = g1 + color[d].getGreen();
+    }
+
+    g1 = g1 / (max);
+    for (d = 0; d < max; d++) {
+     b1 = b1 + color[d].getBlue();
+    }
+
+    b1 = b1 / (max);
+    int sum1 = (a1 << 24) + (r1 << 16)
+      + (g1 << 8) + b1;
+    outputImage.setRGB(y, x, (int) (sum1));
+   }
+  }
+
+  return outputImage;
+ }
+
  public BufferedImage applyFilters(BufferedImage inputImage, int filterInput) {
   switch (filterInput) {
    case 1:
     return toGrayScale(inputImage);
+   case 2:
+    return rotateNintyImage(inputImage);
+   case 3:
+    return horizontalFlip(inputImage);
+   case 4:
+    return verticalFlip(inputImage);
    case 5:
     int increase = new FilterInput().changeBrightness();
     return changeBrightness(inputImage, increase);
+   case 6:
+    return blurImage(inputImage);
   }
 
   return inputImage;
